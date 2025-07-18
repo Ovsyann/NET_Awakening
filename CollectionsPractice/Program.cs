1c0f5562cb7a10106acc82ff4f06541187d7be2a
Ovsyann <oza1999@mail.ru>
2025-07-18 22:43:00 +0300

    SingleNumberOfList

---
+++ CollectionsPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CollectionsPractice
+{
+    internal class Program
+    {
+        private const int ASCII_SHIFT = 48;
+
+        static void Main(string[] args)
+        {
+            List<int> inputList = [];
+            ConsoleKeyInfo keyInfo;
+
+            Console.WriteLine("Input a list");
+
+            while ((keyInfo = Console.ReadKey()).Key != ConsoleKey.Enter)
+            {
+                inputList.Add(keyInfo.KeyChar - ASCII_SHIFT);
+            }
+
+            Console.WriteLine();
+            SingleNumber onlyOneTime = new();
+
+            Console.WriteLine("result: ");
+            foreach (int number in onlyOneTime.GetSingleNumbersOf(inputList))
+            {
+                Console.Write(number);
+            }
+        }
+    }
+}
---
+++ CollectionsPractice/SingleNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CollectionsPractice
+{
+    internal class SingleNumber
+    {
+        private readonly Dictionary<int, int> _numberAndTimesPairs = [];
+        private readonly List<int> _result = [];
+
+        public int[] GetSingleNumbersOf(List<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (_numberAndTimesPairs.ContainsKey(number))
+                {
+                    _numberAndTimesPairs[number] = _numberAndTimesPairs.GetValueOrDefault(number) + 1;
+                }
+                else
+                {
+                    _numberAndTimesPairs.Add(number, 1);
+                }
+            }
+
+            foreach(var key in _numberAndTimesPairs)
+            {
+                if(key.Value == 1)
+                {
+                    _result.Add(key.Key);
+                }
+            }
+
+            return _result.ToArray();
+        }
+    }
+}
